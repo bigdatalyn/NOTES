9d6995f4768d85d222efc9656a10a8935565ebe8
bigdatalyn <88322511@qq.com>
2016-02-22 15:46:20 +0800

    Update Getdbpaths.sql

--- Getdbpaths.sql
+++ Getdbpaths.sql
@@ -1,3 +1,31 @@
+#!/bin/ksh
+
+INT_SEC=3600
+NUM_CYCLE=24
+DATABASE="SAMPLE"
+
+INT_MIN=$(expr ${INT_SEC} / 60)
+CycCount=0
+
+db2 connect to ${DATABASE}
+
+while [[ ${CycCount} -le ${NUM_CYCLE} ]]
+do
+
+  db2 "call monreport.dbsummary(${INT_SEC})" >monreport.dbsummary.$(date "+%Y%m%d_%H%M%S").log
+  db2 "call monreport.pkgcache(${INT_MIN})"  >monreport.pkgcache.$(date "+%Y%m%d_%H%M%S").log
+
+  if [[ $? -gt 0 ]]
+  then
+      echo "MONREPORT procedure execution error occurred!"
+      break
+  fi
+
+  CycCount=$(expr $CycCount + 1)
+  
+done
+
+
 % db2 "select dbpartitionnum, substr(type,1,20) as type, substr(path,1,80) path from sysibmadm.dbpaths"
 
 DBPARTITIONNUM TYPE                 PATH
