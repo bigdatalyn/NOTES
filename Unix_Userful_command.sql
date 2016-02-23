15a305703696839e9e4d63552a57e282f092e601
bigdatalyn <88322511@qq.com>
2016-02-23 13:18:06 +0800

    Update and rename Unix_Userful_command.ksh to Unix_Userful_command.sql

--- Unix_Userful_command.sql
+++ Unix_Userful_command.sql
@@ -19,3 +19,16 @@ du -amx |sort -nr| head -10
 2.2.可以使用dos2unix命令
 
 dos2unix filename
+
+3.标准unix/linux下的grep通过以下参数控制上下文
+
+grep -C 5 foo file 显示file文件中匹配foo字串那行以及上下5行
+
+grep -B 5 foo file 显示foo及前5行
+
+grep -A 5 foo file 显示foo及后5行
+
+
+
+
+
