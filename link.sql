731476a6aaa41a68395d7b95948328512cb72d15
bigdatalyn <88322511@qq.com>
2016-02-23 13:20:43 +0800

    Create link.sql

---
+++ link.sql
@@ -0,0 +1,81 @@
+怎样搭建DB2 DPF for Linux/Unix?
+
+http://blog.163.com/xzp65@126/blog/static/629586972011786135022/    
+
+ 
+
+Linux下NFS(网络文件系统)的建立与配置方法
+
+http://server.zdnet.com.cn/server/2007/0831/482007.shtml
+
+ 
+
+Linux中如何掌握NFS的使用方法
+
+http://www.cnblogs.com/htts/articles/1935487.html
+
+ 
+
+linux配置nfs步骤及心得
+
+http://blog.csdn.net/dongfengkuayue/article/details/27386067
+
+ 
+
+DB2数据库为单个会话锁定技巧
+
+http://www.51cto.com/art/200604/25937.htm
+
+ 
+
+db2cli.ini
+
+ 
+
+DB2 日常维护指南
+
+http://www.ibm.com/developerworks/cn/views/data/libraryview.jsp?search_by=DB2+%E6%97%A5%E5%B8%B8%E7%BB%B4%E6%8A%A4%E6%8C%87%E5%8D%97
+
+ 
+
+DB2 日常维护技巧，第 1 部分
+
+http://www.ibm.com/developerworks/cn/data/library/techarticles/dm-0811chengy4/
+
+ 
+
+DB2 日常维护技巧，第 2 部分
+
+http://www.ibm.com/developerworks/cn/data/library/techarticles/dm-0811chengy5/
+
+ 
+
+ 
+
+db2dart
+
+http://www.ibm.com/developerworks/cn/data/library/techarticles/dm-0906haoqy/
+
+ 
+
+内部体系结构与诊断案例，第 9 章
+
+http://www.ibm.com/developerworks/cn/data/books/db2advan/9/
+
+ 
+
+DB2 性能分析工具介绍：Event Monitor 篇
+
+http://www.ibm.com/developerworks/cn/data/library/techarticle/dm-1112qiaob/
+
+ 
+
+idug  ---important
+
+http://www.idug-db2.com/?commid=71677
+
+ 
+
+health config
+
+/opt/ibm/db2/V9.7fp7/samples/admin_scripts/gethealthconfig.db2
